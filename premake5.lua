caf33781d4702b1dcf2ce8ba9827cd91d0f32ce6
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 14:57:46 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+include "../../premake/common_premake_defines.lua"
+
+project "fmt"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+
+		"./src/os.cc",
+		"./src/format.cc"
+	}
+
+	includedirs {
+		"%{IncludeDir.fmt}"
+	}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
